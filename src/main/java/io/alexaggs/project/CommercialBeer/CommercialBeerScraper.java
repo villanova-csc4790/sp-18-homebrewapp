7e1f89ce7799b5c1789014949840313d0c22cdc1
Alexander Aguilar <aaguila3@villanova.edu>
2018-10-18 17:27:12 -0400

    Changed variable declaration to eliminate redundant storage usage

--- src/main/java/io/alexaggs/project/CommercialBeer/CommercialBeerScraper.java
+++ src/main/java/io/alexaggs/project/CommercialBeer/CommercialBeerScraper.java
@@ -11,15 +11,23 @@ public class CommercialBeerScraper {
     public static ArrayList<CommercialBeer> getTopBeers() throws Exception {
         ArrayList<CommercialBeer> topBeers = new ArrayList<CommercialBeer>();
         int index = 0;
+        String title = "", url = "";
         final Document doc = Jsoup.connect("https://www.beeradvocate.com/lists/top/").get();
+        /*
+         * Accessing the name & style of each top beer
+         */
         for (Element row : doc.select("table td")) {
-            String title = row.select("a b").text();
+            title = row.select("a b").text();
             if (!title.equals("")) {
                 topBeers.add(new CommercialBeer(title, ""));
             }
         }
+
+        /*
+         * Accessing beer profile url extension for each top beer
+         */
         for (Element row : doc.select("table td a")) {
-            String url = row.attr("href");
+            url = row.attr("href");
             if (url.length() > 20 && !(url.length() > 50)) {
                 topBeers.get(index).setUrl(url);
                 index++;
@@ -30,16 +38,16 @@ public class CommercialBeerScraper {
 
     public static ArrayList<String> getBeerData(String url) throws Exception {
         ArrayList<String> beerInfo = new ArrayList<String>();
+        String info = "", text = "", abv = "";
         Document doc = Jsoup.connect(url).get();
         for (Element row : doc.select("div#info_box")) {
-            String info = row.select("a b").text();
-            String text = row.ownText();
-            String abv = text.substring(0, 8);
+            info = row.select("a b").text();
+            text = row.ownText();
+            abv = text.substring(0, 8);
             abv = abv.replace(',', ' ');
             if(!abv.contains("%")) {
                 abv = "No Listed ABV";
             }
-            System.out.println(abv);
             beerInfo.add(info);
             beerInfo.add(abv);
         }
