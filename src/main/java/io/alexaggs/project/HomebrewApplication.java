c32547c33dfee82960e02750c1a73c04d2f2342f
Alexander Aguilar <aaguila3@villanova.edu>
2018-10-25 11:31:15 -0400

    Printing the data for demo

--- src/main/java/io/alexaggs/project/HomebrewApplication.java
+++ src/main/java/io/alexaggs/project/HomebrewApplication.java
@@ -12,20 +12,59 @@ import java.util.ArrayList;
 public class HomebrewApplication {
 
 	public static void main (String args[]) throws Exception {
-		SpringApplication.run(HomebrewApplication.class, args);
+
+		final String IPA_VALUE = "116";
+		final String PILSNER_VALUE = "164";
+
+		//SpringApplication.run(HomebrewApplication.class, args);
 		System.out.println();
-		ArrayList<CommercialBeer> beers = CommercialBeerScraper.getTopBeersByStyle("116");
+
+		System.out.println("------------TOP OVERALL BEERS------------");
+
+		ArrayList<CommercialBeer> topBeers = CommercialBeerScraper.getTopBeersByStyle("116");
 		ArrayList<String> beerInfo;
 		for(int i = 0; i < 10; i++) {
-			beerInfo = CommercialBeerScraper.getBeerData("https://www.beeradvocate.com" + beers.get(i).getUrl());
-			beers.get(i).setCompany(beerInfo.get(0));
-			beers.get(i).setAbv(beerInfo.get(1));
+			beerInfo = CommercialBeerScraper.getBeerData("https://www.beeradvocate.com" + topBeers.get(i).getUrl());
+			topBeers.get(i).setCompany(beerInfo.get(0));
+			topBeers.get(i).setAbv(beerInfo.get(1));
 		}
 
 		for(int i = 0; i < 10; i++) {
-			System.out.println("Name " + beers.get(i).getName());
-			System.out.println("Company " + beers.get(i).getCompany());
-			System.out.println("ABV " + beers.get(i).getAbv());
+			System.out.println("Name: " + topBeers.get(i).getName());
+			System.out.println("Company: " + topBeers.get(i).getCompany());
+			System.out.println("ABV: " + topBeers.get(i).getAbv());
+			System.out.println();
+		}
+
+		System.out.println("------------TOP IPAS------------");
+		ArrayList<CommercialBeer> topIpas = CommercialBeerScraper.getTopBeersByStyle(IPA_VALUE);
+		ArrayList<String> ipaInfo;
+		for(int i = 0; i < 10; i++) {
+			ipaInfo = CommercialBeerScraper.getBeerData("https://www.beeradvocate.com" + topIpas.get(i).getUrl());
+			topIpas.get(i).setCompany(ipaInfo.get(0));
+			topIpas.get(i).setAbv(ipaInfo.get(1));
+		}
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Name: " + topIpas.get(i).getName());
+			System.out.println("Company: " + topIpas.get(i).getCompany());
+			System.out.println("ABV: " + topIpas.get(i).getAbv());
+			System.out.println();
+		}
+
+		System.out.println("------------TOP PILSNERS------------");
+		ArrayList<CommercialBeer> topPilsners = CommercialBeerScraper.getTopBeersByStyle(PILSNER_VALUE);
+		ArrayList<String> pilsnerInfo;
+		for(int i = 0; i < 10; i++) {
+			pilsnerInfo = CommercialBeerScraper.getBeerData("https://www.beeradvocate.com" + topIpas.get(i).getUrl());
+			topPilsners.get(i).setCompany(pilsnerInfo.get(0));
+			topPilsners.get(i).setAbv(pilsnerInfo.get(1));
+		}
+
+		for(int i = 0; i < 10; i++) {
+			System.out.println("Name: " + topPilsners.get(i).getName());
+			System.out.println("Company: " + topPilsners.get(i).getCompany());
+			System.out.println("ABV: " + topPilsners.get(i).getAbv());
 			System.out.println();
 		}
 	}
